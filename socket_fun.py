745bce27b8110d207fcafa26e408f8197f744364
zhe <1655604975@qq.com>
2018-07-26 13:39:20 +0800

    删除不再使用的文件

--- socket_fun.py
+++ socket_fun.py
@@ -20,35 +20,3 @@ def get_host_ip():
     finally:
         s.close()
     return ip
-
-
-def tcplink(sock, addr, db_session):
-    print('新的客户端连接: %s:%s...' % addr)
-    sock.send(('%s:%s OK' % addr).encode())
-    id = '0'
-    while True:
-        try:
-            data = sock.recv(10240)
-        except sock.error:
-            print('socket连接错误')
-            sock.close()
-            break
-        try:
-            if id == '0' and data.decode()[0] == '#' and data.decode()[
-                -1] == '#':
-                id = data.decode()[1:-1]
-                message = id + ' connected'
-                new_msg = Message(message=message)
-            elif id != '0':
-                new_msg = Message(message=id + data.decode())
-            else:
-                print('数据格式错误')
-                continue
-        except:
-            continue
-        db_session.add(new_msg)
-        db_session.commit()
-        sock.send('accept'.encode())
-
-    sock.close()
-    print('客户端 %s:%s 连接断开' % addr)
